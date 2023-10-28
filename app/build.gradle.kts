d11c4eb6b261e7572b698b0e4d647ec30dadcc6b
dntmdwls42 <dntmdwls42@wku.ac.kr>
2023-10-28 14:43:04 +0900

    비밀번호 inputtype 체험, hint추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "kr.ac.wku.signupui_20231028"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "kr.ac.wku.signupui_20231028"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
